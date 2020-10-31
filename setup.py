bd0b63473dbb3aca843533d76fc8f1c2b4669fb2
a-mitani <a-mitani@users.noreply.github.com>
2020-10-31 14:15:17 +0900

    bug fix

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ kwargs = {
     "version": "0.1",
     "description": "Python(numpy) implementation of welford's algorithm.",
     "author": "Akira Mitani",
+    "author_email": "amitani.public@gmail.com",
     "url": "https://github.com/a-mitani/welford",
     "license": "MIT",
     "keywords": ["statistic", "online", "welford"],
     "install_requires": ["numpy"],
+    "packages": ["welford"],
 }
 
 setup(**kwargs)
