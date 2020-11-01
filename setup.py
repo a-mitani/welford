c9e613d37dd9292fac840caeb41bf1e8bb82fafa
a-mitani <a-mitani@users.noreply.github.com>
2020-11-01 16:16:20 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 kwargs = {
     "name": "welford",
-    "version": "0.1.2",
-    "description": "Python(numpy) implementation of welford's algorithm.",
+    "version": "0.1.3",
+    "description": "Python (numpy) implementation of Welford's algorithm.",
     "author": "Akira Mitani",
     "author_email": "amitani.public@gmail.com",
     "url": "https://github.com/a-mitani/welford",
     "license": "MIT",
-    "keywords": ["statistic", "online", "welford"],
+    "keywords": ["statistics", "online", "welford"],
     "install_requires": ["numpy"],
     "packages": ["welford"],
 }
