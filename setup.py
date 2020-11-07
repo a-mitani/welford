961ffe6288154cb35711b0775072b9b7dce2c9b8
a-mitani <a-mitani@users.noreply.github.com>
2020-11-07 10:24:04 +0900

    update setup.py & gitignore files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
 
 kwargs = {
     "name": "welford",
-    "version": "0.2.3",
+    "version": "0.2.4",
     "description": "Python (numpy) implementation of Welford's algorithm.",
     "author": "Akira Mitani",
     "author_email": "amitani.public@gmail.com",
